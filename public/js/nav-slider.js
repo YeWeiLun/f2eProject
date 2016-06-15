27d4e2f492635efeb3c929489c28ed5fc1f36ddd
unknown <wsx741405@gmail.com>
2016-06-15 23:58:36 +0800

    018 navbar 修正鎖框 & 漢堡選單underline ( 缺 dropdown menu style )

--- public/js/nav-slider.js
+++ public/js/nav-slider.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $('#toggle-nav')
       .on('click', function () {
         $('.nav-div').toggleClass('in');
-        $('#wrapper').toggleClass('in');
+        $('body').toggleClass('in');
       });
 });
