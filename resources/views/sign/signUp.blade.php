a8a4b728db7b6c556bc8eb875463934c3ae2c178
Fred50215 <imfred50215@gmail.com>
2016-06-16 22:27:31 +0800

    028_登入註冊介面中文化

--- resources/views/sign/signUp.blade.php
+++ resources/views/sign/signUp.blade.php
@@ -1,23 +1,23 @@
 {{Form::open(array('action'=>'UserController@createUser'))}}
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-    <h4 class="modal-title">SignUp</h4>
+    <h4 class="modal-title">註冊</h4>
 </div>
 <div class="modal-body">
   <p>
-    {{Form::label('Account')}}
+    {{Form::label('帳號')}}
     {{Form::text('acc')}}
   </p>
   <p>
-    {{Form::label('Password')}}
+    {{Form::label('密碼')}}
     {{Form::password('pwd')}}
   </p>
   <p>
-    {{Form::label('Name')}}
+    {{Form::label('姓名')}}
     {{Form::text('name')}}
   </p>
 </div>
 <div class="modal-footer">
-    <button type="button" class="btn btn-primary modal-change-btn" onclick="changeModalView(this);" data-url='sign_in'>Sign In</button>
-    <input type="submit" class="btn btn-primary" value ="Sign Up"></input>
+    <button type="button" class="btn btn-primary modal-change-btn" onclick="changeModalView(this);" data-url='sign_in'>點我登入</button>
+    <input type="submit" class="btn btn-primary" value ="確認送出"></input>
 </div>
