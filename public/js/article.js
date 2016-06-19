30156498c0e0b7ce380072bee6469c32f436523e
unknown <wsx741405@gmail.com>
2016-06-19 22:07:32 +0800

    044 文章  版面大致style

--- public/js/article.js
+++ public/js/article.js
@@ -1,12 +1,15 @@
 // external js: masonry.pkgd.js, imagesloaded.pkgd.js
 
 // init Masonry
-var $grid = $('.grid').masonry({
-  itemSelector: '.grid-item',
-  percentPosition: true,
-  columnWidth: '.grid-sizer'
+
+$(document).ready(function() {
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid-item',
+    percentPosition: true
+    // columnWidth: 200
+  });
 });
 // layout Isotope after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry();
-});
+// $grid.imagesLoaded().progress( function() {
+//   $grid.masonry();
+// });
