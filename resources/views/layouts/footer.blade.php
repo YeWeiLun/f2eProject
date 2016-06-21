b4fed8beed1d3d9ce6dec243d99c8cd1bac272eb
Fred50215 <imfred50215@gmail.com>
2016-06-22 01:24:00 +0800

    071_footer路徑修正

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,13 +1,13 @@
 <footer>
     <div class="div-footerLink">
         <div class="div-footerLinkContent">
-          <a href="member"><span class="span-footerLink">關於我們</span></a>
+          <a href="{{ url('/member') }}"><span class="span-footerLink">關於我們</span></a>
         </div>
         <div class="div-footerLinkContent">
-          <a href="link"><span class="span-footerLink">相關連結</span></a>
+          <a href="{{ url('/link') }}"><span class="span-footerLink">相關連結</span></a>
         </div>
         <div class="div-footerLinkContent">
-          <a href="siteMap"><span class="span-footerLink">網站地圖</span></a>
+          <a href="{{ url('/siteMap') }}"><span class="span-footerLink">網站地圖</span></a>
         </div>
     </div>
     <div class="div-footer">&copy; 2016 Taipei Tech F2E. Fred / Jeff / Wei-Cheng / Wei-Lun
