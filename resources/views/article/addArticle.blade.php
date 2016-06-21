d3950e1c0840f4abba5454ec84ae05eb3ee68e87
unknown <wsx741405@gmail.com>
2016-06-21 23:18:27 +0800

    063 移除文章  版面範例item

--- resources/views/article/addArticle.blade.php
+++ resources/views/article/addArticle.blade.php
@@ -3,6 +3,7 @@
     <h4 class="modal-title">我要發表</h4>
 </div>
 <div class="modal-body">
+  
 </div>
 <div class="modal-footer">
     <button type="button" class="btn btn-info modal-change-btn" onclick="changeModalView(this);" data-url="sign_up">點我註冊</button>
