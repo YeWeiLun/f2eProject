a3e87a5a1a26a33b4b06c668d5113e009a61729f
unknown <wsx741405@gmail.com>
2016-06-22 01:26:52 +0800

    071 sub nav 修改 sub nav dropdown btn close when toggle btn click

--- public/js/nav-slider.js
+++ public/js/nav-slider.js
@@ -3,19 +3,17 @@ $(document).ready(function(){
       .on('click', function () {
         $('.nav-div').toggleClass('in');
         $('body').toggleClass('in');
+        $("div.open").removeClass('open');
+        $('#a-work').removeClass('slider-down');
       });
   $('.dropdown-toggle').on('click', function(){
     $('.dropdown-menu').toggleClass('in');
   })
   $('#article-dropdown-btn').on('click', function(){
-    if($(window).width() <= 767) {
-      $('#a-work').toggleClass('slider-down');
-    }
+    $('#a-work').toggleClass('slider-down');
   })
-  $(document).ready(function(){
-    $(window).resize(function() {
-      $("div.open").removeClass('open');
-      $('#a-work').removeClass('slider-down');
-    });
-});
+  $(window).resize(function() {
+    $("div.open").removeClass('open');
+    $('#a-work').removeClass('slider-down');
+  });
 });
