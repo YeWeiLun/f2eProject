cad2cd71f282e76403e34a1a442697f7c0495631
Fred50215 <imfred50215@gmail.com>
2016-06-22 00:15:00 +0800

    065_addArticle暫時做出頁面

--- resources/views/article/addArticle.blade.php
+++ resources/views/article/addArticle.blade.php
@@ -27,15 +27,11 @@
   @include("layouts.navbar")
   <!-- Content -->
   <div class="div-navWhiteBackGround"></div>
-  <div class="modal-header">
-      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-      <h4 class="modal-title">我要發表</h4>
-  </div>
-  <div class="modal-body">
-  </div>
-  <div class="modal-footer">
-      <button type="button" class="btn btn-info modal-change-btn" onclick="changeModalView(this);" data-url="sign_up">點我註冊</button>
-      <input type="submit" class="btn btn-primary" value ="確認送出"></input>
+  <div class="div-content">
+    <div class="div-titleTexting input-group">
+      <span class="input-group-addon">標題</span>
+      <input type="text" class="form-control"></input>
+    </div>
   </div>
   @include("layouts.footer")
   <!-- 浮框 -->
